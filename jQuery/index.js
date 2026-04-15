114a5fe3b3c01e063e36933d86a890dedef2fd1f
Hyeon-Chea-Hyeon <a01092628769@gmail.com>
2026-04-15 11:36:59 +0900

    Add files via upload

--- jQuery/index.js
+++ jQuery/index.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
     $('#navsub .nsub > li').hide();
     
 
-    $('#nav .nav li').mouseenter(function(e){
-        e.preventDefault();
+    $('#nav .nav li').mouseenter(function(){
         var i = $(this).index();
         $('#navsub .nsub > li').hide();
         $('#navsub .nsub > li').eq(i).fadeIn(200);
@@ -28,4 +27,57 @@ $(document).ready(function(){
         $('#navsub .nsub > li').eq(i).fadeOut();
         // console.log(i)
     });
+
+    var check = $('#bgbtn > li');
+    var vis = $('#bggroup > .bgimg > li');
+    var current = 0;
+    var setintervalId;
+
+    check.click(function(e){
+        e.preventDefault();
+
+        var tg = $(this);
+        var i = $(this).index();
+        // var mo = i*(-100);
+
+        check.removeClass('on');
+        tg.addClass('on');
+
+        move(i);
+    });
+
+    $('#bggroup .bgimg').on({
+        mouseover:function(){
+            clearInterval(setintervalId);
+        },
+        mouseout:function(){
+            time();
+        }
+    });
+
+    time();
+    function time(){
+        setintervalId = setInterval(function(){
+            var n = current+1;
+            if(n==4){
+                n = 0;
+            }
+            check.removeClass('on');
+            check.eq(n).addClass('on');
+            move(n);
+        }, 5000);
+        }
+
+        function move(i){
+            if(current == i) return;
+
+            var ce = vis.eq(current);
+            var ne = vis.eq(i);
+
+            ce.css({left:0}).stop().animate({left:'-100%'});
+            ne.css({left:'100%'}).stop().animate({left:0});
+
+            current = i;
+        }
+    
 });
